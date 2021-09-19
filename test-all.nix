c54e5a582ff2d62a730f88caad026b156f9225e3
Travis Cardwell <travis.cardwell@extrema.is>
2021-09-19 11:13:22 +0900

    Update Nix revisions, use GHC 8.10.7

--- test-all.nix
+++ test-all.nix
@@ -9,6 +9,6 @@
   phatsort-ghc-844 = import ./default.nix { compiler = "ghc844"; };
   phatsort-ghc-865 = import ./default.nix { compiler = "ghc865"; };
   phatsort-ghc-884 = import ./default.nix { compiler = "ghc884"; };
-  phatsort-ghc-8104 = import ./default.nix { compiler = "ghc8104"; };
+  phatsort-ghc-8107 = import ./default.nix { compiler = "ghc8107"; };
   phatsort-ghc-901 = import ./default.nix { compiler = "ghc901"; };
 }
